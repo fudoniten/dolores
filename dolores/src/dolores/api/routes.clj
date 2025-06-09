ea47adb1c750803cb5b329b11d88b9e99a19c134
niten <niten@fudo.org>
2025-06-09 09:37:20 -0700

    refactor: Simplify route definitions by removing unused function and imports

--- dolores/src/dolores/api/routes.clj
+++ dolores/src/dolores/api/routes.clj
@@ -1,5 +1,5 @@
 (ns dolores.api.routes
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [routes GET]]
             [compojure.route :as route]
             [ring.util.response :as response]))
 
@@ -9,7 +9,3 @@
     (GET "/emails/summary" [] (response/response "Quick summary of recent emails"))
     (GET "/emails/important" [] (response/response "Summary of emails to read or reply to"))
     (route/not-found "Not Found")))
-
-(defn define-routes []
-  ;; Function to define API routes
-  )
